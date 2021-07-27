ef889d4d9b732facdb8157ae5c34f9bb2e90409f
tanveertkd <hazarikatanveer12@gmail.com>
2021-07-27 14:44:38 +0530

    done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # neog-markEight
-An emoji interpreter app created using React.js
+## Emoji interpreter
+<br />
+An emoji interpreter app created using React.js. You can either click on one of the displayed emojis or input your own to know the meaning of it.
+
+Assignment: markEight
+
--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function App() {
 
   const emojiInputHandler = (event) => {
     const userInput = event.target.value;
-    const emojiMeaning = foodmojiDictionary[userInput];
+    let emojiMeaning = foodmojiDictionary[userInput];
     if (emojiMeaning === undefined) {
       emojiMeaning = "Can't find the meaning in dictionary. Sorry!";
     }
@@ -37,8 +37,20 @@ export default function App() {
   // const keys = Object.keys(foodmojiDictionary);
   return (
     <div className="App">
-      <h1>Foodmoji</h1>
-      <input id="user-input" onChange={emojiInputHandler}></input>
+      <nav className="navigation">
+        <p className="nav-header">Foodmoji</p>
+      </nav>
+
+      <div className="primary-header">
+        <h1>Foodmoji</h1>
+        <p>An interpreter that displays the meaning of emojis</p>
+      </div>
+
+      <input
+        id="user-input"
+        placeholder="Enter emoji"
+        onChange={emojiInputHandler}
+      ></input>
 
       <div id="show-selected-emojiname">{selectedEmoji}</div>
 
@@ -46,9 +58,10 @@ export default function App() {
         id="emoji-table"
         style={{
           display: "grid",
-          gridTemplateRows: "50px 50px 50px",
-          gridTemplateColumns: "50px 50px 50px",
-          justifyContent: "center"
+          // gridTemplateRows: "50px 50px 50px",
+          // gridTemplateColumns: "50px 50px 50px",
+          // justifyContent: "center"
+          gridTemplateColumns: "repeat(3, 1fr)"
         }}
       >
         {
@@ -68,6 +81,16 @@ export default function App() {
           // })
         }
       </div>
+
+      <footer className="footer">
+        <div className="footer-header">
+          <p>The emoji tranlator!</p>
+          <p>Use it to know the meanings of the emojis.</p>
+          <p>
+            <i className="fa fa-copyright"></i> | 2021
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
